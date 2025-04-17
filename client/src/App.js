e6cc21f43ace302149379e63508d5c121b2e1449
dakodab <dakotabragato@gmail.com>
2025-04-17 23:39:48 +0100

    Fix: remove unused axios import for production build

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import axios from 'axios'; // not currently being used? //
+// import axios from 'axios'; // not currently being used? //
 import LondonMap from './components/london.map.experimental';
 
 
