cffbca9cb0c2d7b16874f77d0ed14aec174bb61f
dakodab <dakotabragato@gmail.com>
2025-05-04 13:24:53 +0100

    Switch to MySQL connection pool using mysql2

--- server/db.js
+++ server/db.js
@@ -1,12 +1,15 @@
 const mysql = require('mysql2');
-require('dotenv').config(); // <-- Make sure .env loads here too
+require('dotenv').config(); // <-- Make sure .env loads here too ("environment variables" - secret configuration values like password, keys, etc.)
 
-const connection = mysql.createConnection({
+const pool = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  port: parseInt(process.env.DB_PORT) || 3306
+  port: parseInt(process.env.DB_PORT) || 3306,
+  waitForConnections: true,
+  connectionLimit: 10, // arbitrary - could change //
+  queueLimit: 0
 });
 
-module.exports = connection;
+module.exports = pool; // removed .promise()
